11a536f116cf6a6581146a74b3da38b14fdbb656
Theran Brigowatz <theran.brigowatz@gmail.com>
2019-06-02 00:02:44 +0900

    added error page

---
+++ pages/_error.js
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import { Ghost } from 'react-kawaii';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Router from 'next/router';
+
+const ErrorStyles = styled.div`
+  background-color: #232323;
+  height: 100vh;
+  margin: 0 auto;
+  text-align: center;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr;
+  align-items: center;
+  h2,
+  h1 {
+    font-family: 'Raleway', sans-serif;
+  }
+  h1 {
+    font-size: 3rem;
+    color: #40a4c8;
+  }
+  h2 {
+    color: gainsboro;
+  }
+  div .main-content {
+    background: green;
+  }
+`;
+
+const ErrorPage = props => {
+  const [isHappy, setIsHappy] = useState(false);
+  return (
+    <ErrorStyles>
+      <Head>
+        <title>Oops...Something Went Wrong</title>
+      </Head>
+      <div className="main-content">
+        <h1>Oops...Something went wrong.</h1>
+        <h1>Are you sure you're in the right place?</h1>
+        <h2>This is Hank. He is feeling blue that you are in the wrong place.</h2>
+        <h2>However if you poke his belly you can cheer him up and he might take you back home.</h2>
+        <div
+          onClick={() => {
+            setIsHappy(true);
+            setTimeout(() => {
+              Router.push({ pathname: '/' });
+            }, 1000);
+          }}
+        >
+          <Ghost size={200} mood={isHappy ? 'blissful' : 'sad'} color={isHappy ? 'Gainsboro' : '#40a4c8'} />
+        </div>
+      </div>
+    </ErrorStyles>
+  );
+};
+
+export default ErrorPage;
--- pages/project.js
+++ pages/project.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Browser } from 'react-kawaii';
 import ProjectLayout from '../components/ProjectLayout';
 import data from '../static/data';
+import ErrorPage from './_error';
 
 const Project = ({ query }) => {
   const [project, setProject] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     data.forEach(item => {
       if (item.slug === query.name) {
         setProject({ ...item });
       }
     });
+    setLoading(false);
   }, []);
-
+  if (loading) return <Browser size={200} mood="blissful" color="#40a4c8" />;
+  if (!loading && !Object.keys(project).length) return <ErrorPage />;
   return <ProjectLayout project={project} />;
 };
 
