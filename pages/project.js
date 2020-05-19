0c3b59a7c1c79bd7dac03c92d0267fdcb861225a
Theran <theran.brigowatz@gmail.com>
2020-05-19 09:43:23 +0900

    updated package.json

--- pages/project.js
+++ pages/project.js
@@ -1,43 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
 import ErrorPage from './_error';
-import LoadingStyles from '../components/styles/LoadingStyles';
 import ProjectLayout from '../components/ProjectLayout';
 import PropTypes from 'prop-types';
 import data from '../static/data';
 import { motion } from 'framer-motion';
 
 const Project = ({ query, projectInfo }) => {
-  const [project, setProject] = useState(projectInfo ? projectInfo : {});
-  const [loading, setLoading] = useState(true);
+  const [project] = useState(projectInfo ? projectInfo : {});
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   data.forEach((item) => {
-  //     if (item.slug === query.name) {
-  //       setProject({ ...item });
-  //     }
-  //   });
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 1000);
-  // }, []);
-
-  // if (loading)
-  //   return (
-  //     <LoadingStyles>
-  //       <motion.div
-  //         exit={{ opacity: 0, scale: 0 }}
-  //         initial={{ opacity: 0, scale: 0.8 }}
-  //         animate={{ opacity: 1, scale: 1 }}
-  //         transition={{ type: 'spring', ease: 'easeIn', duration: 1, mass: 0.5 }}>
-  //         <div>
-  //           {/* <Browser size='80%' mood='blissful' color='#40a4c8' /> */}
-  //           <h1>Loading...</h1>
-  //         </div>
-  //       </motion.div>
-  //     </LoadingStyles>
-  //   );
   if (!projectInfo) return <ErrorPage />;
 
   return (
