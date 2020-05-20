d3391a402d7eb1c664515edd5874ec636c9f1300
Theran <theran.brigowatz@gmail.com>
2020-05-20 13:27:27 +0900

    service worker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
 // next.config.js
 const withImages = require('next-images');
 const withBabelMinify = require('next-babel-minify');
+const withOffline = require('next-offline');
+
+module.exports = withOffline({
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT ? 'service-worker.js' : 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+  experimental: {
+    async rewrites() {
+      return [
+        {
+          source: '/service-worker.js',
+          destination: '/_next/static/service-worker.js',
+        },
+      ];
+    },
+  },
+});
 
 module.exports = withBabelMinify();
 
