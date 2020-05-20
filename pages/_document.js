aafa4a67f2a42cd3f602fed111e5b6db111651f3
Theran <theran.brigowatz@gmail.com>
2020-05-20 13:06:09 +0900

    update manifest

--- pages/_document.js
+++ pages/_document.js
@@ -47,6 +47,10 @@ class MyDocument extends Document {
           <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
           <link rel='manifest' href='/static/manifest.json' />
           <link rel='icon' type='image/x-icon' href='../static/favicon.ico' prefetch />
+          <link rel='apple-touch-icon' sizes='180x180' href='../static/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='../static/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='../static/favicon-16x16.png' />
+          <link rel='manifest' href='../static/site.webmanifest' />
           {this.props.styleTags}
         </Head>
         <body style={{ margin: '0', width: '100%' }}>
