618342adca5213199c2f06d9837fde60e68ec44d
Theran <theran.brigowatz@gmail.com>
2020-06-04 13:03:37 +0900

    updated public routes

--- pages/_document.js
+++ pages/_document.js
@@ -45,12 +45,12 @@ class MyDocument extends Document {
             content='https://res.cloudinary.com/dq7uyauun/image/upload/q_auto/v1551968239/IMG_1880.jpg'></meta>
           <meta name='description' content='Theran Brigowatz - Web Developer Portfolio Site' />
           <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
-          <link rel='manifest' href='/static/manifest.json' />
-          <link rel='icon' type='image/x-icon' href='../static/favicon.ico' />
+          <link rel='manifest' href='/public/manifest.json' />
+          <link rel='icon' type='image/x-icon' href='../public/favicon.ico' />
           <link rel='apple-touch-icon' sizes='180x180' href='../static/apple-touch-icon.png' />
-          <link rel='icon' type='image/png' sizes='32x32' href='../static/favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='../static/favicon-16x16.png' />
-          <link rel='manifest' href='../static/site.webmanifest' />
+          <link rel='icon' type='image/png' sizes='32x32' href='../public/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='../public/favicon-16x16.png' />
+          <link rel='manifest' href='../public/site.webmanifest' />
           {this.props.styleTags}
         </Head>
         <body style={{ margin: '0', width: '100%' }}>
