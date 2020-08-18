abc9f70bba1e37dbd618aaa0292ab44bf16cafb8
Theran <theran.brigowatz@gmail.com>
2020-08-18 16:38:52 +0900

    on change analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import App from 'next/app';
 import React from 'react';
+import Router from 'next/router';
 
+import * as gtag from '../lib/gtag';
+
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url));
 export default class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "15d598a792b5ba602322d4b792eb0566"
+    "revision": "f294ac6eb269575c34a9236654d7e6fa"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
